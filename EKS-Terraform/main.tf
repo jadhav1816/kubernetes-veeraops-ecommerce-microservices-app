f71af4075156357b1852c6ebfbe69e45d15ebf9a
jadhav1816 <65172963+jadhav1816@users.noreply.github.com>
2026-08-21 14:15:35 +0530

    Update main.tf

--- EKS-Terraform/main.tf
+++ EKS-Terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 #############################
@@ -47,7 +47,7 @@ resource "aws_subnet" "public1" {
 
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
@@ -55,7 +55,7 @@ resource "aws_subnet" "public2" {
 
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-2b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
@@ -63,14 +63,14 @@ resource "aws_subnet" "private1" {
 
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "private2" {
 
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-west-2b"
+  availability_zone = "us-east-1b"
 }
 
 ############################
