a0a8315f3044a7abe1608a92aacb9977ba018720
jadhav1816 <65172963+jadhav1816@users.noreply.github.com>
2026-08-21 13:42:09 +0530

    Update backend.tf

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "devncloudtechdevops"
     key          = "terraform.tfstate"
-    region       = "us-east-1"
+    region       = "us-west-2"
     use_lockfile = true
   }
 }
